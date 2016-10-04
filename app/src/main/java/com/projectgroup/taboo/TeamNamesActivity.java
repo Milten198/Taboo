095446db6b8a701a4e6e7dd5eda22d8165e69fc6
lukasz <lukasz.frydrych@performgroup.com>
2016-10-04 13:55:42 +0200

    Changed target version sdk

---
+++ app/src/main/java/com/projectgroup/taboo/TeamNamesActivity.java
@@ -0,0 +1,13 @@
+package com.projectgroup.taboo;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TeamNamesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
