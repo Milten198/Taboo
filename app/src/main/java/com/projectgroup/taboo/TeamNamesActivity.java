303ebfe52d2f898092c7defe7f19ba80e81cf73c
Tomasz Kubit <tomasz.kubit@performgroup.com>
2016-10-13 16:16:54 +0200

    Add TeamNamesActivity and according files

--- app/src/main/java/com/projectgroup/taboo/TeamNamesActivity.java
+++ app/src/main/java/com/projectgroup/taboo/TeamNamesActivity.java
@@ -1,8 +1,18 @@
 package com.projectgroup.taboo;
 
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+/**
+ * Created by tomasz.kubit on 2016-10-04.
+ */
 
 public class TeamNamesActivity extends Activity {
 
@@ -10,5 +20,16 @@ public class TeamNamesActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_names);
+    }
+
+    public void addMorePlayers_redTeam(View view) {
+
+    }
+
+    public void addMorePlayers_blueTeam(View view) {
+
+    }
+
+    public void goFurther(View view) {
     }
 }
