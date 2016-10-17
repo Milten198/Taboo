997baa85c610eb5faa45c1a812dc1e1006148b92
Tomasz Kubit <tomasz.kubit@performgroup.com>
2016-10-17 15:23:45 +0200

    Modify page to add players

--- app/src/main/java/com/projectgroup/taboo/TeamNamesActivity.java
+++ app/src/main/java/com/projectgroup/taboo/TeamNamesActivity.java
@@ -1,14 +1,14 @@
 package com.projectgroup.taboo;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.support.v7.app.AppCompatActivity;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by tomasz.kubit on 2016-10-04.
@@ -16,18 +16,54 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class TeamNamesActivity extends Activity {
 
+    EditText namesBlueTeam;
+    Button addPlayersBlueTeam;
+    LinearLayout containerBlueTeam;
+    EditText namesRedTeam;
+    Button addPlayersRedTeam;
+    LinearLayout containerRedTeam;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_names);
-    }
-
-    public void addMorePlayers_redTeam(View view) {
-
+        namesBlueTeam = (EditText)findViewById(R.id.first_player_blue);
+        namesRedTeam = (EditText)findViewById(R.id.first_player_red);
+        addPlayersBlueTeam = (Button)findViewById(R.id.more_players_blue_team);
+        addPlayersRedTeam = (Button)findViewById(R.id.more_players_red_team);
+        containerBlueTeam = (LinearLayout)findViewById(R.id.container_blue);
+        containerRedTeam = (LinearLayout)findViewById(R.id.container_red);
     }
 
     public void addMorePlayers_blueTeam(View view) {
+        addPlayers(namesBlueTeam, containerBlueTeam);
+    }
 
+    public void addMorePlayers_redTeam(View view) {
+        addPlayers(namesRedTeam, containerRedTeam);
+    }
+
+    public void addPlayers(EditText team, LinearLayout container) {
+        LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View addView = layoutInflater.inflate(R.layout.activity_to_add_more_players, null);
+        TextView textOut = (TextView)addView.findViewById(R.id.textout);
+        textOut.setText(team.getText().toString());
+        Button buttonRemove = (Button)addView.findViewById(R.id.remove);
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((LinearLayout)addView.getParent()).removeView(addView);
+            }
+        });
+        container.addView(addView);
+    }
+
+    public void clearEditText_redTeam(View view) {
+        namesRedTeam.setText("");
+    }
+
+    public void clearEditText_blueTeam(View view) {
+        namesBlueTeam.setText("");
     }
 
     public void goFurther(View view) {
