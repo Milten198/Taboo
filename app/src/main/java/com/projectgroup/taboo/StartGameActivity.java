9eed3320548bcfffd5380f0eca9321b3a54bee3f
Tomasz Kubit <tomasz.kubit@performgroup.com>
2016-11-23 12:30:28 +0100

    Create BeforeStartActivity

--- app/src/main/java/com/projectgroup/taboo/StartGameActivity.java
+++ app/src/main/java/com/projectgroup/taboo/StartGameActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.StringBuilderPrinter;
 import android.view.View;
 import android.widget.TextView;
 
@@ -21,6 +22,8 @@ public class StartGameActivity extends AppCompatActivity {
     ArrayList<String> names_blueTeam;
     private int teamBluePoints = 0;
     private int teamRedPoints = 0;
+    TextView scoresRedTeam;
+    TextView scoresBlueTeam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +33,19 @@ public class StartGameActivity extends AppCompatActivity {
         names_redTeam = (ArrayList<String>) getIntent().getSerializableExtra("names_redTeam");
         names_blueTeam = (ArrayList<String>) getIntent().getSerializableExtra("names_blueTeam");
 
+        scoresRedTeam = (TextView) findViewById(R.id.score_red_team);
+        scoresBlueTeam = (TextView) findViewById(R.id.score_blue_team);
+
         textOut.setText("Czerwoni: " + names_redTeam + " ;;;;;; " + ", Niebiescy: " + names_blueTeam);
 
-        //setTeamPoints();
+
+
+        setTeamPoints();
     }
 
     private void setTeamPoints() {
-        TextView scoresRedTeam = (TextView) findViewById(R.id.score_red_team);
-        TextView scoresBlueTeam = (TextView) findViewById(R.id.score_blue_team);
-        scoresBlueTeam.setText(teamBluePoints);
-        scoresRedTeam.setText(teamRedPoints);
+        scoresBlueTeam.setText(teamBluePoints +"");
+        scoresRedTeam.setText(teamRedPoints+"");
     }
 
 
