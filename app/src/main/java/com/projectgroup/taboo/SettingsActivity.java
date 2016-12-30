f70576c234e61145dfbec0ac0b048cb8de82a0b8
Tomasz Kubit <tomaszkubit@L01007871.local>
2016-12-30 13:40:34 +0100

    Add spinners and checkboxes

--- app/src/main/java/com/projectgroup/taboo/SettingsActivity.java
+++ app/src/main/java/com/projectgroup/taboo/SettingsActivity.java
@@ -3,11 +3,42 @@ package com.projectgroup.taboo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SettingsActivity extends Activity {
+
+    private Spinner spinner$_points_to_win;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        init();
     }
+
+    private void init() {
+        spinner$_points_to_win = (Spinner) findViewById(R.id.spinner$_points_to_win);
+        spinner$_points_to_win.setSelection(3);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.points_to_win, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner$_points_to_win.setAdapter(adapter);
+        spinner$_points_to_win.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+
 }
